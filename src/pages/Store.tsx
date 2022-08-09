898af65f69be8b952248139c6572e1a9db1606ca
Soyeb <tssoyeb@gmail.com>
2022-08-10 03:24:35 +0600

    implement store and storeItem component with type

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,10 +1,18 @@
+import storedItems from "../data/items.json"
+import { Col, Row } from "react-bootstrap";
+import StoreItem from "../component/StoreItem";
 
 
 const Store = () => {
     return (
-        <div>
+        <>
             <h3>Store</h3>
-        </div>
+            <Row md={2} xs={1} lg={3} className="g-3">
+                {storedItems.map(item=>(
+                    <Col key={item.id}><StoreItem {...item}/></Col>
+                ))}
+            </Row>
+        </>
     );
 };
 
