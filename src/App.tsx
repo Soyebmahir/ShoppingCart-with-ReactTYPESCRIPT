b9f40cd45ffab92467795c97ebac636bf36b662e
Soyeb <tssoyeb@gmail.com>
2022-08-18 21:02:23 +0600

    create function under shoppingCartContext component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import { Container } from "react-bootstrap"
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./component/Navbar"
+import { ShoppingCartProvider } from "./contest/ShoppingCartContext"
 import About from "./pages/About"
 import Home from "./pages/Home"
 import Store from "./pages/Store"
 
 
 function App() {
- 
 
-  return (<>
-    <Navbar></Navbar>
-    <Container>
-      <h1>HI</h1>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/store" element={<Store/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-      </Routes>
-    </Container>
-    </>
+
+  return (
+    <ShoppingCartProvider>
+      <Navbar></Navbar>
+      <Container>
+        <h1>HI</h1>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   )
 }
 
